bd52f749e1240240cc2649f4c6ad773dd0c06dff
al-amin90 <ijesun30@gmail.com>
2026-04-03 19:51:30 +0600

    feat: config and mongoose db connected successfully

---
+++ src/lib/db/mongodb.ts
@@ -0,0 +1,38 @@
+import config from "@/config";
+import mongoose from "mongoose";
+
+const MONGODB_URI = config.database_url as string;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+// deleclaring mongoose variable in global
+declare global {
+  var mongoose: {
+    conn: mongoose.Connection | null;
+    promise: Promise<mongoose.Connection> | null;
+  };
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect(): Promise<mongoose.Connection> {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m.connection);
+  }
+
+  cached.conn = await cached.promise;
+
+  console.log("jit");
+
+  return cached.conn;
+}
+
+export default dbConnect;
